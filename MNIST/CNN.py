40b71c468d6ce850d3864d5064ff28d859783d9f
jaejin1 <opiximeo@gmail.com>
2017-07-26 12:42:57 +0900

    MNIST

---
+++ MNIST/CNN.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import random
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+nb_classes = 10
+
+X = tf.placeholder(tf.float32, [None, 784])
+X_img = tf.reshape(X,[-1, 28, 28, 1])  #28x28x1
+Y = tf.placeholder(tf.float32, [None, nb_classes])
+
+W1 = tf.Variable(tf.random_normal([3,3,1,32], stddev=0.01))
+
+L1 = tf.nn.conv2d(X_img, W1, strides=[1,1,1,1], padding='SAME')
+L1 = tf.nn.relu(L1)
+L1 = tf.nn.max_pool(L1, ksize=[1,2,2,1], strides=[1,2,2,1],padding='SAME')
+
+W2 = tf.Variable(tf.random_normal([3,3,32,64], stddev=0.01))
+
+L2 = tf.nn.conv2d(L1, W2, strides=[1,1,1,1], padding='SAME')
+L2 = tf.nn.relu(L2)
+L2 = tf.nn.max_pool(L2, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+L2 = tf.reshape(L2, [-1,7*7*64])
+
+
+W3 = tf.get_variable("W2", shape=[7*7*64, 10],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b = tf.Variable(tf.random_normal([10]))
+hypothesis = tf.matmul(L2, W3) + b
+
+
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
+
+optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
+
+is_correct = tf.equal(tf.arg_max(hypothesis, 1), tf.arg_max(Y, 1))
+
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+
+training_epochs = 15=
+batch_size = 100
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+print('Learning stared')
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        feed_dict = {X: batch_xs, Y: batch_ys}
+        c, _, = sess.run([cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c/ total_batch
+    print('Epoch:', '%04d' % (epoch + 1), 'cost = ', '{:.9f}'.format(avg_cost))
+
+print('Learning Finished!')
+
+
+print('Accuracy:', sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels}))
